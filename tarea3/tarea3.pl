51dc86c7858d4eed1ff16dc2725abc96b6b9fa45
adam3497 <amador19972012@gmail.com>
2023-03-04 18:38:52 -0600

    Función Unión, Intersección y Diferencia Prolog

---
+++ tarea3/tarea3.pl
@@ -0,0 +1,31 @@
+/*
+    Tarea corta 3 
+    Lenguajes de Programación Gr 40
+    Escuela de Computación - Instituto Tecnológico de Costa Rica
+    I Semestre 2023, Centro Académico de San José
+    Profesor: José Rafael Castro Mora
+    Estudiante: Adrián Amador Ávila
+*/
+
+/* Funcion union de conjuntos */
+union([], [], []) :- !.
+union([], List2, List2) :- !.
+union([H|T], List2, List3) :-
+    (member(H, List2)) -> 
+        union(T, List2, List3)
+    ;
+        List3 = [H|Res],
+        union(T, List2, Res).
+
+/* Función intersección de conjuntos */
+intersec([], [], []) :- !.
+intersec([], _, []) :- !.
+intersec([H|T], List2, List3) :-
+    (member(H, List2)) ->
+        List3 = [H|Res], 
+        intersec(T, List2, Res)
+    ;
+        intersec(T, List2, List3).
+
+
+/* Función diferencia de conjuntos */
