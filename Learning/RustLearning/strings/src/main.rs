0bab43cdf383cc9214bf05b14718e058d5511888
adam3497 <amador19972012@gmail.com>
2023-05-24 14:30:04 -0600

    Rust handling Strings

--- Learning/RustLearning/strings/src/main.rs
+++ Learning/RustLearning/strings/src/main.rs
@@ -20,5 +20,28 @@ fn main() {
     s.push('l');
     println!("{s}");
 
+    // concatenation with the + Operator or the format! macro
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
 
+    println!("s1 + s2 = {s3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("s = {s}");
+
+    // Iterating over String
+    // using chars to get each individual character (notice here each character takes 2 bytes)
+    for c in "Зд".chars() {
+        println!("{c}");
+    }
+
+    // bytes method returns each raw byte 
+    for b in "Зд".bytes() {
+        println!("{b}");
+    }
 }
