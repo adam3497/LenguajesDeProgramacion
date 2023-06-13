20ffdb8f223f26333d9bcd7a34c7781b485b02ff
adam3497 <amador19972012@gmail.com>
2023-06-13 08:43:03 -0600

    Learning to do some tests in Rust

--- Learning/RustLearning/adder/src/lib.rs
+++ Learning/RustLearning/adder/src/lib.rs
@@ -60,4 +60,27 @@ mod tests {
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
     }
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+
+        let v1_iter = v1.iter();
+
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
 }
